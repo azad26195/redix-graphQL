997f7b35c8453920b9de673baa647bf2c7929570
azad <azad26195@gmail.com>
2016-10-05 12:33:31 +0530

    added graphQl model type

--- server.js
+++ server.js
@@ -52,3 +52,30 @@ var goldbergs = {
    id: 6
  }
 }
+
+var goldbergType = new GraphQLObjectType({
+  name: "Goldberg",
+  description: "Member of The Goldbergs",
+  fields: {
+   character: {
+     type: GraphQLString,
+     description: "Name of the character",
+   },
+   actor: {
+     type: GraphQLString,
+     description: "Actor playing the character",
+   },
+   role: {
+     type: GraphQLString,
+     description: "Family role"
+   },
+   traits: {
+     type: GraphQLString,
+     description: "Traits this Goldberg is known for"
+   },
+   id: {
+     type: GraphQLInt,
+     description: "ID of this Goldberg"
+   }
+ }
+});
